565f7f50fc71c037bf6a814cf4f75d4f6e655a88
denisweb <denisweb@MacBook-Air-denisweb.local>
2020-02-09 19:22:12 +0300

    вывод верстки с помощью js

---
+++ js/modules/cards.js
@@ -0,0 +1,13 @@
+const books = [{
+        "uri": "klienty-na-vsyu-zhizn",
+        "name": "Клиенты на всю жизнь",        
+        "price": 1000,
+        "number": 2
+    },
+    {
+        "uri": "tsennye-resheniya",
+        "name": "Ценные решения",
+        "price": 1000,
+        "number":1
+    },
+];
--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
 ready(function(){
 
   // В этом месте должен быть написан ваш код
+let cardsHTML = '';
 
+function createCardHTML(card) {
+  return `<tr class="cart__product">
+                <td class="cart__col-1">
+                  <img class="cart__item-img" src="img/${card.uri}.jpg">
+                </td>
+                <td class="cart__col-2">
+                  <div class="cart__item-name">${card.name}</div>
+                </td>
+                <td class="cart__col-3">
+                  <div class="field-num  field-num--bg-tran">
+                    <span class="field-num__input-wrap">
+                      <button class="field-num__btn-minus" type="button">-</button>
+                      <input class="field-num__input" type="number" value="2" step="1" min="1"/>
+                      <button class="field-num__btn-plus" type="button">+</button>
+                    </span>
+                  </div>
+                </td>
+                <td class="cart__col-4">
+                  <span class="cart__item-price">${card.price}₽</span>
+                </td>
+                <td class="cart__col-5">
+                  <button class="close cart__product-del-btn" type="button">
+                    <svg width="16" height="16">
+                      <use xlink:href="#close"></use>
+                    </svg>
+                  </button>
+                </td>
+              </tr>`;
+}
+
+for (let book of books) {
+  cardsHTML = cardsHTML + createCardHTML(book);
+}
+
+document.getElementById('js-cart__table').innerHTML = cardsHTML;
 
 
   // ВНИМАНИЕ!
